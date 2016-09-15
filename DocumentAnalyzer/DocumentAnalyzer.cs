c098f75caea9276c3efbe1ec6cee6b3803461c80
Joe <jozwludz@gmail.com>
2016-09-15 20:28:03 +0200

    Created solution and projects in Visual Studio.

---
+++ DocumentAnalyzer/DocumentAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategoriseUsingSVM
+{
+    public class DocumentAnalyzer
+    {
+        // ToDo - Create a mechanism that will gather all document and compute them into vectors.
+        // Should contain a base dictionary to create points on the plane thanks by created vectors.
+    }
+}
---
+++ DocumentAnalyzer/SupportVectorMachine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategoriseUsingSVM
+{
+    public class SupportVectorMachine
+    {
+        // ToDo - Should poses a function to categorise document on the plane.
+        // Need to create an idea how to do it.
+    }
+}
