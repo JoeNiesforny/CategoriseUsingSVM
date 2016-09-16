6d4546f0fa308b9d7bd1f146ff4646b6a026167c
Jozef <joze32@gmail.com>
2016-09-17 00:35:06 +0200

    Added mechanism to gather and compute data from files.
    ToDo
    -> Add support for database.
    -> Improve comparing dictionary words with data words.
    -> Implement SVM.
    -> Create a GUI.

--- DocumentAnalyzer/DocumentAnalyzer.cs
+++ DocumentAnalyzer/DocumentAnalyzer.cs
@@ -1,14 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace CategoriseUsingSVM
 {
+    public class Document
+    {
+        public string Path;
+        public bool[] Vector;
+        public Point Position;
+        public string Category;
+
+        public Document(string path, bool[] vector)
+        {
+            Path = path;
+            Vector = vector;
+            var count = vector.Count() / 2;
+            if (vector.Count() % 2 == 1)
+                count++;
+            Position = new Point(SumOfElementLessThan(vector, count),
+                                 SumOfElementMoreThanAndEqual(vector, count));
+        }
+
+        private int SumOfElementLessThan(bool[] vector, int count)
+        {
+            int sum = 0;
+            for (int iter = 0; iter < count; iter++)
+                if (vector[iter])
+                    sum += (int)Math.Pow(2, iter);
+            return sum;
+        }
+
+        private int SumOfElementMoreThanAndEqual(bool[] vector, int count)
+        {
+            int sum = 0;
+            for (int iter = count; iter < vector.Count(); iter++)
+                if (vector[iter])
+                    sum += (int)Math.Pow(2, iter - count);
+            return sum;
+        }
+    }
+
     public class DocumentAnalyzer
     {
-        // ToDo - Create a mechanism that will gather all document and compute them into vectors.
-        // Should contain a base dictionary to create points on the plane thanks by created vectors.
+        public List<string> Dictionary;
+        public List<Document> Documents;
+
+        public DocumentAnalyzer()
+        {
+            Dictionary = new List<string> ();
+            Documents = new List<Document> ();
+        }
+
+        public DocumentAnalyzer(string[] dictionary)
+        {
+            Dictionary = new List<string>(dictionary);
+            Documents = new List<Document>();
+        }
+
+        public DocumentAnalyzer(string[] dictionary, string[] files)
+        {
+            Dictionary = new List<string>(dictionary);
+            Documents = new List<Document>();
+            foreach (var file in files)
+                AddNewDocument(file);
+        }
+
+        public bool AddNewDocument(string path)
+        {
+            path = path.Trim();
+            foreach (var document in Documents)
+                if (path == document.Path)
+                    return false;
+            Documents.Add(new Document(path, CountVector(path)));
+            return true;
+        }
+
+        private bool[] CountVector(string path)
+        {
+            var vector = new bool[Dictionary.Count];
+            var data = File.ReadAllText(path);
+            for (var iter = 0; iter < Dictionary.Count; iter++)
+                if (data.IndexOf(Dictionary[iter]) >= 0)
+                    vector[iter] = true;
+            return vector;
+        }
     }
 }
--- DocumentAnalyzerTests/DocumentAnalyzerTests.cs
+++ DocumentAnalyzerTests/DocumentAnalyzerTests.cs
@@ -1,13 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CategoriseUsingSVM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Windows;
 
 namespace DocumentAnalyzerTests
 {
+    [TestClass]
     public class DocumentAnalyzerTests
     {
-        // ToDo - Choose a framework for tests.
+        string Path;
+        string Data;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Path = "tmp-document";
+            Data =
+                "Training a support vector machine (SVM) leads to" +
+                " a quadratic optimization problem with bound constraints" +
+                " and one linear equality constraint.";
+            File.WriteAllText(Path, Data);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            if (File.Exists(Path))
+                File.Delete(Path);
+        }
+
+        [TestMethod]
+        public void AddNewDocument_ValidateDocumentProperties()
+        {
+            var dictionary = new string[] { "Training", "SVM", "neural", "network", "quadratic", "optimization"};
+            var analyzer = new DocumentAnalyzer(dictionary);
+            Assert.IsTrue(analyzer.AddNewDocument(Path));
+            CollectionAssert.AreEqual(analyzer.Documents[0].Vector,
+                new bool[] { true, true, false, false, true, true });
+            Assert.AreEqual(analyzer.Documents[0].Position, new Point(3,6));
+        }
+
+        [TestMethod]
+        public void AddNewDocument_DictionaryCountIsOdd_ValidateDocumentProperties()
+        {
+            var dictionary = new string[] { "Training", "SVM", "neural", "network", "quadratic", "optimization", "linear" };
+            var analyzer = new DocumentAnalyzer(dictionary);
+            Assert.IsTrue(analyzer.AddNewDocument(Path));
+            CollectionAssert.AreEqual(analyzer.Documents[0].Vector,
+                new bool[] { true, true, false, false, true, true, true });
+            Assert.AreEqual(analyzer.Documents[0].Position, new Point(3, 7));
+        }
     }
 }
